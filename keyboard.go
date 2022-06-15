fa9a33767ea1a36ff72cd37f7cd05b81a68e5984
Reda Ghoussoub <redaghoussoub@Redas-MacBook-Pro.local>
2022-06-15 18:46:25 -0400

    returned float 0.0 if error

--- keyboard.go
+++ keyboard.go
@@ -12,14 +12,14 @@ func GetFloat() (float64, error) {
 	input, err := reader.ReadString('\n')
 
 	if err != nil {
-		return 0, err
+		return 0.0, err
 	}
 
 	input = strings.TrimSpace(input)
 	number, err := strconv.ParseFloat(input, 64)
 
 	if err != nil {
-		return 0, err
+		return 0.0, err
 	}
 
 	return number, nil
