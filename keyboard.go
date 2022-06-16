2a2132718c6e32026d49c48c0d6c016b0f51e1b1
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2022-06-15 21:28:05 -0400

    return 0 instead of 0.0 in GetFloat

--- keyboard.go
+++ keyboard.go
@@ -15,14 +15,14 @@ func GetFloat() (float64, error) {
 	input, err := reader.ReadString('\n')
 
 	if err != nil {
-		return 0.0, err
+		return 0, err
 	}
 
 	input = strings.TrimSpace(input)
 	number, err := strconv.ParseFloat(input, 64)
 
 	if err != nil {
-		return 0.0, err
+		return 0, err
 	}
 
 	return number, nil
